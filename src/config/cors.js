819618dd20df0ff13dcca81cbc5adeb5ff35632c
KarlosAZ <carlossayubu@matriz.com.es>
2025-03-05 09:40:54 +0100

    [ADD] Correcion cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,13 +1,11 @@
 const corsOptions = {
     origin: ["http://localhost:3000", "https://api.coreerp.matriz.com.es", "http://api.coreerp.matriz.com.es"],
 
-    // Métodos HTTP permitidos
+
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 
-    // Encabezados permitidos
     allowedHeaders: ["Content-Type", "Authorization"],
 
-    // Permite cookies y credenciales
     credentials: true,
 
     maxAge: 86400,
